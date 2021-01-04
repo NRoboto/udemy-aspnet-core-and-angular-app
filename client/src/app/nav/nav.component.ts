ddf899a6f4501688c3a710e7c075b81d99ae05a7
Mike <mnightingale@tutanota.com>
2021-01-04 14:25:43 +0000

    Add client toast for errors

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,16 +13,19 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    this.accountService.login(this.model).subscribe(response => {
+    this.accountService.login(this.model).subscribe((response) => {
       this.router.navigateByUrl("/members");
-    }, error => {
+    }, (error) => {
       console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
